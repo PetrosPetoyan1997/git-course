89f7e76dc61afbd7744523a7882e5ead841309a1
Petros <petoyanpetros97@gmail.com>
2022-04-26 19:12:46 +0400

    commit from another computer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 console.log("test")
+console.log("new console loges")
+console.log("new console loges")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
