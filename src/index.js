ab71f67bcea9877ac07570733345bc254fd99fdc
Petros <petoyanpetros97@gmail.com>
2022-04-27 00:54:15 +0400

    test message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+console.log("3333333333333")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
