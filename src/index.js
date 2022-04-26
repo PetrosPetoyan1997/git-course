044e563abf62446b2b5c12654036ba7ef51d04af
Petros2 <petros.petoyan97@mail.ru>
2022-04-26 21:38:51 +0400

    from another file 2222

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 console.log("test")
 console.log("new console loges")
 console.log("new console loges")
+console.log("111111111111")
+console.log("111111111111")
+console.log("111111111111")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
