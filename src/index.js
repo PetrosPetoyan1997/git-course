d5cbae3e9d68457f8cfc4739af27756aa97ac929
Petros <petoyanpetros97@gmail.com>
2022-04-26 22:01:25 +0400

    delete all console logs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-console.log("test")
-console.log("new console loges")
-console.log("new console loges")
-console.log("111111111111")
-console.log("111111111111")
-console.log("111111111111")
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
