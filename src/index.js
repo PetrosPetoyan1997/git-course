13664434fd6f6e06faa8ae46b56732e5461fb80e
Petros <petoyanpetros97@gmail.com>
2022-04-27 15:01:34 +0400

    add App.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 console.log("3333333333333")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App/>
   </React.StrictMode>
 );
 
