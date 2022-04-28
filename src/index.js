e20125deae88bb97eb74afabbea7d07248da3809
Petros2 <petros.petoyan97@mail.ru>
2022-04-28 22:20:23 +0400

    tew

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-console.log(false)
+console.log(true)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
