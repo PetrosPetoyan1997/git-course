6ac762cef1035af6676e1af5bd3bdf6702088c1e
Petros2 <petros.petoyan97@mail.ru>
2022-04-29 18:57:59 +0400

    message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-console.log('index.js')
+console.log('7777777777777777777777777.js')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
